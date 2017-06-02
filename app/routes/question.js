002f3e9fbb174e0342e65d6f6c229deb8017d3da
sowmyadsl <sowmya.dsl@gmail.com>
2017-06-02 16:07:15 -0700

    Add functionality to delete each answer associated with that question

--- app/routes/question.js
+++ app/routes/question.js
@@ -14,8 +14,26 @@ export default Ember.Route.extend({
       question.save();
       this.transitionTo('index');
     },
+    saveAnswer(params){
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function(){
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    },
     destroyQuestion(question){
       question.destroyRecord();
+      this.transitionTo('index');
+    },
+    destroyAnswer(answer){
+      var answer_deletions = question.get('answers').map(function(answer){
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function(){
+        return question.destroyRecord();
+      });
       this.transitionTo('index');
     }
   }
