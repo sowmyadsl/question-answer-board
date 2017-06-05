7d522cac38291bad39b6537fdb446b15f9b9d973
sowmyadsl <sowmya.dsl@gmail.com>
2017-06-04 18:04:07 -0700

    Add styling and fonts to the page

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,7 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 content: DS.attr(),
 author: DS.attr(),
-upvotes: DS.attr(),
-downvotes: DS.attr(),
 question: DS.belongsTo('question', {async: true }),
 });
